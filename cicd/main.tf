c5cc5e2938f4f05e82eac106ec04105dbc9a7e9d
Oskar Conislla <oskarconislla20@gmail.com>
2025-08-06 19:23:20 -0500

    Update main.tf

--- cicd/main.tf
+++ cicd/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "rg" { #rg2 son solo nombres
 }
 
 resource "azurerm_storage_account" "storage" {
-  name = "continousdeployment"
+  name = "continousdeployosk"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   account_tier = "Standard"
